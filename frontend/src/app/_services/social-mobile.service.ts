1e1e22a24f6f2b047adf53be0acfd846e34ed6a2
Pete Vieira <pete.vieira@languageconservancy.com>
2025-10-28 16:15:26 -0700

    fix!: upgrade to capacitor 6 and plugins to appropriate versions
    - also disable add friend via facebook due to constant api/sdk changes testing complexity
    - using limited login flow per apple requirements, decoding jwt in frontend

--- frontend/src/app/_services/social-mobile.service.ts
+++ frontend/src/app/_services/social-mobile.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@angular/core";
 import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
-import { FacebookLogin } from "@capacitor-community/facebook-login";
+import { FacebookLogin, FacebookLoginResponse } from "@capacitor-community/facebook-login";
 
 import { environment } from "environments/environment";
+
+interface LimitedLoginClaims {
+    sub: number;
+    name: string;
+    email: string;
+}
 
 @Injectable({
     providedIn: "root",
@@ -12,7 +18,7 @@ export class SocialMobileService {
 
     initGoogle() {
         GoogleAuth.initialize({
-            clientId: `${environment.GOOGLE_CLIENT_ID_WEB}`,
+            clientId: `${environment.GOOGLE_CLIENT_ID_IOS}`,
             scopes: ["profile", "email"],
             grantOfflineAccess: true,
         });
@@ -38,38 +44,61 @@ export class SocialMobileService {
     }
 
     async signInWithFacebook(): Promise<any> {
-        const result = await FacebookLogin.login({ permissions: ["public_profile", "email"] });
+        // Check if user has a current access token
+        // let result: FacebookLoginResponse = await FacebookLogin.getCurrentAccessToken();
+        // if (!result?.accessToken?.token) {
+        console.log("No current access token, logging in...");
+        let result: FacebookLoginResponse = await FacebookLogin.login({
+            permissions: ["public_profile", "email"],
+        });
 
-        if (!result || !result.accessToken) {
-            console.error("Facebook login failed.");
-            throw new Error("Facebook login failed.");
+        const jwt = result?.accessToken?.token;
+        if (!jwt) {
+            throw new Error("Facebook login failed. No JWT found in login response.");
         }
+
+        const claims = this.decodeLimitedLoginJwt(jwt);
+        console.debug("Decoded claims", claims);
+
         // Facebook login successful
-        const fbUser = await this.getFacebookUserProfile();
-        if (!fbUser) {
-            throw new Error("Failed to get Facebook user profile");
-        }
+        // const userProfileJson = await fetch(
+        // `https://graph.facebook.com/me?fields=id,name,email&access_token=${result?.accessToken?.token}`,
+        // );
+        // const userProfile = await userProfileJson.json();
+        // if (!userProfile) {
+        // throw new Error("Failed to get Facebook user profile");
+        // }
 
-        return this.extractFacebookUserData(fbUser);
+        return this.extractFacebookUserData(claims);
     }
 
-    async getFacebookUserProfile() {
-        const result = await FacebookLogin.getProfile<{
-            id: number;
-            name: string;
-            email: string;
-        }>({ fields: ["id", "name", "email"] });
-        return result;
+    decodeLimitedLoginJwt(jwt: string): LimitedLoginClaims {
+        const [, payload] = jwt.split(".");
+        const base64 = payload.replace(/-/g, "+").replace(/_/g, "/");
+        const json = decodeURIComponent(
+            atob(base64)
+                .split("")
+                .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+                .join(""),
+        );
+        return JSON.parse(json) as LimitedLoginClaims;
     }
 
     /**
+     * Extracts necessary data from Facebook JWT to send to our
+     * login endpoint.
      */
-    extractFacebookUserData(user: any): any {
+    extractFacebookUserData(claims: LimitedLoginClaims): {
+        type: string;
+        social_id: number;
+        name: string;
+        email: string;
+    } {
         return {
             type: "fb",
-            social_id: user.id,
-            name: user.name,
-            email: user.email,
+            social_id: claims.sub,
+            name: claims.name,
+            email: claims.email,
         };
     }
 }
